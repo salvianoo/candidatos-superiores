63d065bfc5a0345a86a2e7288ca7666ec360cd29
Salviano Ludgério <salviano.lfg@gmail.com>
2014-09-15 11:57:32 -0300

    Update script.rb

--- script.rb
+++ script.rb
@@ -3,4 +3,4 @@ require_relative 'dados_dos_deputados'
 script = DadosDosDeputados.new
 
 puts script.federais_com_superior_completo
-# puts script.estaduais_com_superior_completo
+puts script.estaduais_com_superior_completo
