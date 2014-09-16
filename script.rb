f4d7946a66ac82bea7ad67e6812c17526cf235ef
Salviano Ludgério <salviano.lfg@gmail.com>
2014-09-16 16:24:19 -0300

    Minor: clean

--- script.rb
+++ script.rb
@@ -2,10 +2,10 @@ require_relative 'dados_dos_deputados'
 
 dados = DadosDosDeputados.new
 
-File.open('federais.txt', 'w') do |f1|
-  dados.federais_com_superior_completo.each { |line| f1.puts line }
+File.open('federais.txt', 'w') do |f|
+  dados.federais_com_superior_completo.each { |line| f.puts line }
 end
 
-File.open('estaduais.txt', 'w') do |f2|
-  dados.estaduais_com_superior_completo.each { |line| f2.puts line }
+File.open('estaduais.txt', 'w') do |f|
+  dados.estaduais_com_superior_completo.each { |line| f.puts line }
 end
