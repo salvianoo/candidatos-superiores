710361c1e6c7c8bfac8c9fda139bdfa7ab6e231e
Salviano Ludgério <salviano.lfg@gmail.com>
2014-09-16 09:48:20 -0300

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,11 +1,11 @@
 require_relative 'dados_dos_deputados'
 
-script = DadosDosDeputados.new
+dados = DadosDosDeputados.new
 
 File.open('federais.txt', 'w') do |f1|
-  script.federais_com_superior_completo.each { |line| f1.puts line }
+  dados.federais_com_superior_completo.each { |line| f1.puts line }
 end
 
 File.open('estaduais.txt', 'w') do |f2|
-  script.estaduais_com_superior_completo.each { |line| f2.puts line }
+  dados.estaduais_com_superior_completo.each { |line| f2.puts line }
 end
